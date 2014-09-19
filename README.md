5f6da5ed8dd6efb31bc830a0cc66673b7a4af0a8
golemboy <golemboy@gmail.com>
2014-09-19 10:16:56 +0200

    last commit

---
+++ README.md
@@ -0,0 +1,15 @@
+This repository contains the following files:
+
+  - README.md
+  - plot1.R to plot6.R    
+  - plot1.png to plot6.png
+
+Before run the R script, please : 
+
+  1) download the data from : https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip
+  
+  2) unzip in your working directory
+  
+  3) Open the R script ```plot*.r``` using a text editor.
+  
+  4) uncomment and change the parameter of the setwd function
---
+++ plot6.R
@@ -0,0 +1,33 @@
+##Compare emissions from motor vehicle sources in Baltimore City with emissions from motor vehicle sources in Los Angeles County, California (fips == "06037"). 
+##Which city has seen greater changes over time in motor vehicle emissions?
+
+require("plyr")
+require("ggplot2")
+
+#set path
+#setwd("/your/working/directory")
+
+## This first line will likely take a few seconds. Be patient!
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+cities <- data.frame(c("24510","06037"),c("Baltimore City","Los Angeles County"))
+colnames(cities) <- c("fips","City")
+
+#use Emission in Baltimore City and Los Angeles County
+bc_la_NEI <- NEI[NEI$fips %in% cities$fips & NEI$type == "ON-ROAD",]
+bc_la_NEI <- merge(bc_la_NEI, cities, by = "fips")
+
+#aggregate with ddply by year and City
+aggreg <- ddply(bc_la_NEI, c("year","City"), summarise, amount=sum(Emissions))
+breaks <- as.vector(unique(aggreg$year))
+
+#plot6
+png(filename = "plot6.png", width = 480, height = 480, units = "px")
+g <- ggplot(aggreg, aes(x = year, y = amount, fill = factor(year))) + theme(legend.position='none')
+g <- g + geom_bar(stat="identity")
+g <- g + facet_wrap(~ City)
+g <- g + scale_x_continuous(breaks=breaks)
+g <- g + ggtitle('PM2.5 Emission per Type in Baltimore City') + ylab("PM2.5 Emission (Tons)") + xlab("Year")
+g
+dev.off()
